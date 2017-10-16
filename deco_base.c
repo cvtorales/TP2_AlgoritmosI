2721153dedbc6f06dfad71318de6d72f0c9673bc
agustinpiperno <31783815+agustinpiperno@users.noreply.github.com>
2017-10-16 17:43:49 -0300

    Add files via upload

---
+++ deco_base.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "funciones.h"
+#include "common.h"
+
+int main (int argc, char *argv[])
+{
+	FILE* fentrada, fsalida;
+	char* salida_csv;
+
+	if((st = validar_argumentos_deco_base(argc,argv,&fentrada))!=ST_OK)
+		{
+			fprintf(stderr, "Error en alguna validacion\n");
+			return EXIT_FAILURE;
+		}
+
+	copy_bin_to_csv(argv[1],salida_csv);
+
+	return ST_OK;
+}
+
+
+
+
