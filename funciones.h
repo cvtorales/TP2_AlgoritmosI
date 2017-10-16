bac5c6f31c55b31a444d32d49b29d82963e64526
agustinpiperno <31783815+agustinpiperno@users.noreply.github.com>
2017-10-16 17:47:02 -0300

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -8,7 +8,7 @@ status_t split(const char * cadena, char delimitador, char *** arreglo, size_t *
 status_t strdup(const char*sc, char **p);
 status_t destruir_arreglo_cadenas(char *** campos, size_t n);
 status_t cargar_datos(juego_t *ptr_juego,char **arreglo, size_t l);
-status_t copy_bin_to_csv(char* entrada, char* salida)
+status_t copy_bin_to_csv(char* entrada, char* salida);
 
 status_t validar_argumentos_deco_base(int argc, char *argv[], FILE **fentrada);
 status_t validar_argumentos(int argc, char *argv[], FILE **fentrada);
