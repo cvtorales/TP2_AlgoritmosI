1b0a951f5b9a0fcc532f766ae30730d727a558da
cvtorales <31779598+cvtorales@users.noreply.github.com>
2017-10-17 13:26:01 +0200

    Add files via upload
    tiene las funciones para altas bajas o modificacioes

--- puntaje.c
+++ puntaje.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "common.h"
 
 #define CANTIDAD_DE_ARGUMENTOS_PUNTAJE 8
-#define NOMBRE_ARCHIVO_BASE_DE_DATOS "entrada.txt"
-#define NOMBRE_ARCHIVO_A_MODIFICAR "salida.txt"
+#define NOMBRE_ARCHIVO_BASE_DE_DATOS "data.bin"
+#define NOMBRE_ARCHIVO_A_MODIFICAR "db.bin"
 #define NOMBRE_ARCHIVO_PARA_MSJS_DE_ERRORES "errores.txt"
 /*************** NUMERO DE POSICIONES PARA ARGUMENTOS ******************/
 #define ARG_CMD_NUMERO_DE_POSICION_OPERACION 1
@@ -15,47 +16,47 @@
 #define ARG_CMD_NUMERO_DE_POSICION_BANDERA_ARCHIVO_MSJS_ERROR 6
 #define ARG_CMD_NUMERO_DE_POSICION_NOMBRE_ARCHIVO_MSJS_ERROR 7
 /***********************************************************************/
-typedef enum {
 
-				ST_OK,
-				ST_ERROR_NO_MEM,
-				ST_ERROR_PUNTERO_NULO,
-				ST_ERROR_CARGAR_ID,
-				ST_ERROR_CARGAR_FECHA,
-				ST_ERROR_CARGAR_PUNTAJE,
-				ST_ERROR_CARGAR_RESENIAS,
-				ST_ERROR_CANT_ARG,
-				ST_ERROR_APERTURA_ARCHIVO,
-				ST_ERROR_CANTIDAD_INVALIDA_DE_ARGUMENTOS,
-	/**************** ERRORES PARA APP PUNTAJE ***********************/
-				ST_ERROR_CMD_ARG_OPERACION,
-				ST_ERROR_CMD_ARG_BANDERA_PARA_ARCHIVO_BASE_DE_DATOS,
-				ST_ERROR_CMD_ARG_NOMBRE_ARCHIVO_BASE_DATOS_INVALIDO,
-				ST_ERROR_CMD_ARG_BANDERA_PARA_ARCHIVO_A_MODIFICAR,
-				ST_ERROR_CMD_ARG_NOMBRE_ARCHIVO_A_MODIFICAR_INVALIDO,
-				ST_ERROR_CMD_ARG_BANDERA_PARA_ARCHIVO_MSJS_DE_ERRORES,
-				ST_ERROR_CMD_ARG_NOMBRE_ARCHIVO_MSJ_DE_ERRORES_INVALIDO,
-				ST_ERROR_APERTURA_ARCHIVO_DE_ENTRADA,
-				ST_ERROR_APERTURA_ARCHIVO_DE_SALIDA
-
-	/*****************************************************************/			
-			} status_t;
 
 status_t validar_argumentos_puntaje (int argc, char * argv[], FILE **fentrada, FILE ** fsalida);
-
+status_t procesar_altas(FILE ** fentrada, FILE ** fsalida, juego_t * ptr_juego_data, juego_t * ptr_juego_db );
+status_t procesar_bajas(FILE ** fentrada, FILE ** fsalida, juego_t * ptr_juego_data, juego_t * ptr_juego_db );
+status_t procesar_modificaciones(FILE ** fentrada, FILE ** fsalida, juego_t * ptr_juego_data, juego_t * ptr_juego_db );
 
 int main (int argc, char *argv[])
 {
-	FILE *fentrada;
-	FILE *fsalida;
-	status_t st;
+	FILE *data;   /* Registros para dar alta, baja o modificaciones */
+	FILE *db;     /*  Base de datos */
+	status_t st; 
 
-		if((st = validar_argumentos_puntaje(argc, argv, &fentrada, &fsalida))!=ST_OK)
+	juego_t * ptr_juego_db,  * ptr_juego_data;
+	juego_t juego_db, juego_data;
+	ptr_juego_data= &juego_data;
+	ptr_juego_db= &juego_db;
+
+	/*size_t id_aux_data;
+	size_t id_aux_db;
+*/
+		if((st = validar_argumentos_puntaje(argc, argv, &data, &db))!=ST_OK)
 	{
 		fprintf(stderr, "Error en alguna validacion\n");
 		return EXIT_FAILURE;
 	}
 
+/*switch()   me falta poner este switch, para el caso de altas,bajas o modificaciones
+	{
+		case "A": 
+		case "B":
+		case "M":
+	}*/
+	if((procesar_altas(&data, &db, ptr_juego_data, ptr_juego_db ))!=ST_OK)
+		return EXIT_FAILURE;
+
+	if((procesar_bajas(&data, &db, ptr_juego_data, ptr_juego_db ))!=ST_OK)
+		return EXIT_FAILURE;
+
+	if((procesar_modificaciones(&data, &db, ptr_juego_data, ptr_juego_db ))!=ST_OK)
+		return EXIT_FAILURE;
 
 	return EXIT_SUCCESS;
 }
@@ -74,7 +75,9 @@ status_t validar_argumentos_puntaje (int argc, char * argv[], FILE **fentrada, F
 		return ST_ERROR_CANTIDAD_INVALIDA_DE_ARGUMENTOS; 
 	
 /********************** Se validan las banderas ****************************/	
-	if(	strcmp(argv[ARG_CMD_NUMERO_DE_POSICION_OPERACION], "A") && strcmp(argv[1], "B") &&	strcmp(argv[1], "M") )
+	if(	strcmp(argv[ARG_CMD_NUMERO_DE_POSICION_OPERACION], "A") 
+		&& strcmp(argv[ARG_CMD_NUMERO_DE_POSICION_OPERACION], "B")
+		 &&	strcmp(argv[ARG_CMD_NUMERO_DE_POSICION_OPERACION], "M") )
 		return ST_ERROR_CMD_ARG_OPERACION;
 
 
@@ -96,12 +99,12 @@ status_t validar_argumentos_puntaje (int argc, char * argv[], FILE **fentrada, F
 	if (strcmp(argv[ARG_CMD_NUMERO_DE_POSICION_NOMBRE_ARCHIVO_MSJS_ERROR], NOMBRE_ARCHIVO_PARA_MSJS_DE_ERRORES))
 		return ST_ERROR_CMD_ARG_NOMBRE_ARCHIVO_MSJ_DE_ERRORES_INVALIDO;
 
-		if((*fentrada = fopen(argv[3],"rt"))==NULL)
+		if((*fentrada = fopen(argv[3],"rb"))==NULL)
 	{
 		printf("entre a ST_ERROR_APERTURA_ARCHIVO_ENTRADA\n");
 		return ST_ERROR_APERTURA_ARCHIVO_DE_ENTRADA;
 	}
-			if((*fsalida = fopen(argv[5],"rt"))==NULL)
+			if((*fsalida = fopen(argv[5],"ab"))==NULL)
 	{
 		printf("entre a ST_ERROR_APERTURA_ARCHIVO_SALIDA\n");
 		return ST_ERROR_APERTURA_ARCHIVO_DE_SALIDA;
@@ -109,5 +112,113 @@ status_t validar_argumentos_puntaje (int argc, char * argv[], FILE **fentrada, F
 
 /********************* Fin de validacion de banderas ***********************/
 		
+	return ST_OK;
+}
+
+status_t procesar_altas(FILE ** fentrada, FILE ** fsalida, juego_t * ptr_juego_data, juego_t * ptr_juego_db )
+{
+				while(fread(&((ptr_juego_data)->id),sizeof(size_t),1, *fentrada) != EOF)
+				{	
+					while(fread(&((ptr_juego_db)->id),sizeof(size_t),1, *fsalida) != EOF )
+					{		
+						if(((ptr_juego_data)->id) == ((ptr_juego_db)->id))
+							return ST_ERROR_ID_REPETIDO;
+
+						fseek(*fsalida,SEEK_END,0);
+						fwrite(ptr_juego_db,sizeof(juego_t),1, *fsalida);
+					}
+				}
+	return ST_OK;
+}
+
+status_t procesar_bajas(FILE ** fentrada, FILE ** fsalida, juego_t * ptr_juego_data, juego_t * ptr_juego_db )
+{
+	FILE * temp;
+	
+	while(fread(&((ptr_juego_db)->id),sizeof(size_t),1, *fsalida) != EOF )
+	{	
+		while(fread(&((ptr_juego_data)->id),sizeof(size_t),1, *fentrada) != EOF)
+		{			
+			if(((ptr_juego_data)->id) != ((ptr_juego_db)->id))
+			{
+			
+				if((temp=fopen("temp.bin","wb"))==NULL)
+				{
+					printf("entre a error de archivo temporal\n");
+					return ST_ERROR_APERTURA_ARCHIVO_DE_ENTRADA;
+				}
+				
+					fwrite(ptr_juego_db,sizeof(juego_t),1, temp);
+			
+			}
+
+		}
+	}
+
+	fclose(*fsalida);
+	fclose(*fentrada);
+
+	if((*fsalida=fopen("db.bin","wb"))==NULL)
+	{
+		printf("entre a error de archivo temporal\n");
+		return ST_ERROR_APERTURA_ARCHIVO_DE_ENTRADA;
+	}
+
+	while(fread(&ptr_juego_db,sizeof(juego_t),1, temp) != EOF )
+	{
+		fwrite(ptr_juego_db,sizeof(juego_t),1, *fsalida);
+	}	
+
+	fclose(*fsalida);
+	fclose(temp);
+
+	return ST_OK;
+}
+
+
+status_t procesar_modificaciones(FILE ** fentrada, FILE ** fsalida, juego_t * ptr_juego_data, juego_t * ptr_juego_db )
+{
+	FILE * temp;
+	
+	while(fread(&((ptr_juego_db)->id),sizeof(size_t),1, *fsalida) != EOF )
+	{	
+		while(fread(&((ptr_juego_data)->id),sizeof(size_t),1, *fentrada) != EOF)
+		{			
+			if(((ptr_juego_data)->id) != ((ptr_juego_db)->id))
+			{
+			
+				if((temp=fopen("temp.bin","ab"))==NULL)  /* puse ab porque escribo dos veces */
+				{
+					printf("entre a error de archivo temporal\n");
+					return ST_ERROR_APERTURA_ARCHIVO_DE_ENTRADA;
+				}
+				
+					fwrite(ptr_juego_db,sizeof(juego_t),1, temp);
+			
+			}else
+			{
+				fwrite(ptr_juego_data,sizeof(juego_t),1, temp);
+			}
+
+		}
+	}
+
+	fclose(*fsalida);
+	fclose(*fentrada);
+
+	if((*fsalida=fopen("db.bin","wb"))==NULL)
+	{
+		printf("entre a error de archivo temporal\n");
+		return ST_ERROR_APERTURA_ARCHIVO_DE_ENTRADA;
+	}
+
+	while(fread(&ptr_juego_db,sizeof(juego_t),1, temp) != EOF )
+	{
+		fwrite(ptr_juego_db,sizeof(juego_t),1, *fsalida);
+	}	
+
+	fclose(*fsalida);
+	fclose(temp);
+
 	return ST_OK;
 }
